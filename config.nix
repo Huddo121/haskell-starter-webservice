0905ee4fdcddb36ac4f05dc59d330d049ceb05a7
Michael Hudson <huddo121@gmail.com>
2018-11-30 22:59:41 +1100

    Update to GHC 8.4.4 and HIE 0.3.0.0

--- config.nix
+++ config.nix
@@ -1,6 +1,6 @@
 rec {
-  ghcVersion = "ghc843";
-  nixpkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/tarball/a7fd4310c0cc7f50d2e5eb1f6172c31969569930) {
+  ghcVersion = "ghc844";
+  nixpkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/tarball/c1acb5ce6d8472891d3748e86ac0ff0f17de4ed9) {
     config = {
       allowUnfree = true;
       packageOverrides = pkgs: rec {
@@ -14,5 +14,5 @@ rec {
       };
     };
   };
-  hie = import (fetchTarball https://github.com/domenkozar/hie-nix/tarball/96af698f0cfefdb4c3375fc199374856b88978dc) { pkgs=nixpkgs; };
+  hie = import (fetchTarball https://github.com/domenkozar/hie-nix/tarball/a270d8db4551f988437ac5db779a3cf614c4af68) { };
 }
