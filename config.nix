35a7b9aa0f84ac39fab3e7fa9719541d19ebd01f
Michael Hudson <huddo121@gmail.com>
2019-06-10 12:12:34 +1000

    Backport more comprehensive config.nix from other projects. Remove PROJECT_NAME.nix and ./bin/udpate-derivation
    No need to manually update the derivation any more since I'm using callCabal2Nix.

--- config.nix
+++ config.nix
@@ -1,18 +1,54 @@
 rec {
-  ghcVersion = "ghc844";
-  nixpkgs = import (fetchTarball https://github.com/NixOS/nixpkgs/tarball/c1acb5ce6d8472891d3748e86ac0ff0f17de4ed9) {
-    config = {
-      allowUnfree = true;
-      packageOverrides = pkgs: rec {
-        hPkgs = pkgs.haskell.packages.${ghcVersion};
+  # Which version of GHC does the project need to be built with? This must exist in both nixpkgs, and hie-nix
+  ghcVersion = "ghc864";
+  # This should be compatible with the above ghcVersion
+  hieVersion = "hie86";
+  # Bootstrap the ability to fetch from GitHub
+  fetchFromGitHub = (import <nixpkgs> {}).fetchFromGitHub;
 
-        haskellPackages = hPkgs.override {
+  # The set of haskellPackages we are working with. This lists packages that can be built with our specified
+  #  version of GHC
+  hpkgs = nixpkgs.haskell.packages.${ghcVersion};
+
+  # Build up a nix expression for this project from the cabal file
+  drv = nixpkgs.haskellPackages.callCabal2nix "haskell-starter-webservice" ./. {};
+
+  # Repositories and commits that will be used in this build. Using specific commits and hashes increases
+  #  the reproducibility and cacheability of builds.
+  # In order to retrieve the hashes for these repos run
+  #  nix-prefetch-url --unpack https://github.com/$OWNER/$REPO/archive/$REV.tar.gz
+  repos = {
+    # A specific commit of the nixpkgs repository we wish to build from, for maximum repeatability
+    nixpkgs = {
+      owner = "NixOs";
+      repo = "nixpkgs";
+      rev = "5e7e5a2";
+      sha256 = "1m1ic0wgr2y0qq6iglpav1ajh6zcx2x4hk2fhf1n0p0h73v3601p";
+    };
+
+    # The version of hie-nix we wish to include with the project
+    hie-nix = {
+      owner="domenkozar";
+      repo="hie-nix";
+      rev="922bbc7";
+      sha256="1wf80g1zbgglc3lyqrzfdaqrzhdgmzhgg1p81hd2cpp57gpai9wh";
+    };
+  };
+
+  # Our custom copy of nixpkgs, with our project's package injected in to the set of haskellPackages
+  nixpkgs = import (fetchFromGitHub repos.nixpkgs) {
+    config = {
+      # allowUnfree = true;
+      packageOverrides = pkgs: rec {
+        haskellPackages = hpkgs.override {
           overrides = super: self: {
-            haskell-starter-webservice = self.callPackage ./haskell-starter-webservice.nix { };
+            haskell-starter-webservice = drv;
           };
         };
       };
     };
   };
-  hie = import (fetchTarball https://github.com/domenkozar/hie-nix/tarball/a270d8db4551f988437ac5db779a3cf614c4af68) { pkgs=nixpkgs; };
+
+  # The version of haskell-ide-engine we are using
+  hie = (import (fetchFromGitHub repos.hie-nix) { }).${hieVersion};
 }
--- shell.nix
+++ shell.nix
@@ -1,18 +1,6 @@
-{
-  config ? import (./config.nix)
-}:
-with config.nixpkgs;
 let
-  # Import the package and all its dependencies
-  package = (import ./.);
-
-  hie = config.hie.hie84;
-
-  hpkgs = haskell.packages.${config.ghcVersion};
-
-  # Set up a shell environment with all those dependencies and more!
-  shellEnvironment = package.env.overrideAttrs (originalAttrs: {
-    buildInputs = originalAttrs.buildInputs ++ [ hie cabal2nix cabal-install hlint hpkgs.ghcid hpkgs.stylish-haskell hpkgs.hasktags hpkgs.hoogle ];
-  });
-
-in shellEnvironment
+  config = (import ./config.nix);
+in with config; hpkgs.shellFor {
+  packages = p: [drv];
+  buildInputs = with nixpkgs; [ hie cabal2nix cabal-install hlint hpkgs.ghcid hpkgs.stylish-haskell hpkgs.hasktags hpkgs.hoogle ];
+}
